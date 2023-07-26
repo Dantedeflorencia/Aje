349fb5a0304334309c0908993d1f51812e17aba9
Dantedeflorencia <140614068+Dantedeflorencia@users.noreply.github.com>
2023-07-26 19:57:36 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
-document.write("Lo que me parecia muy dificil ha sido la solicion");
-var $nombre =("Dante");
-document.write($nombre);
-document.write("trabajando desde githup, cosa bastante complicada");
+// CODAI ^-^
+
+// Define a function to add spaces between lines of text
+function addSpacesBetweenLines(text) {
+  // Split the input text into an array of lines
+  const lines = text.split('\n');
+
+  // Join the lines with spaces in between and return the result
+  return lines.join(' ');
+}
+
+// Example usage
+const inputText = `Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt`;
+
+const result = addSpacesBetweenLines(inputText);
+console.log(result);

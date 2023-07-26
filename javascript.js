389cf51b2a6499a262865035876fdecb7bad6907
Dantedeflorencia <140614068+Dantedeflorencia@users.noreply.github.com>
2023-07-26 15:00:51 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 document.write("este es el nuevo servidor Appserv y parece que esta un pocoLOCO");
 var $nombre =("Dante");
 document.write($nombre);
-document.write("desde github");
+document.write("trabajando desde githup, cosa bastante complicada");

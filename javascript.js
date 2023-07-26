e2cc58c4a6a40779597d2802074ee33623b34bd7
Dantedeflorencia <140614068+Dantedeflorencia@users.noreply.github.com>
2023-07-26 20:07:49 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,18 +1,6 @@
-// CODAI ^-^
+/// Definimos el mensaje que queremos imprimir
+var mensaje = "¡Hola, este es un mensaje en pantalla!";
 
-// Define a function to add spaces between lines of text
-function addSpacesBetweenLines(text) {
-  // Split the input text into an array of lines
-  const lines = text.split('\n');
-
-  // Join the lines with spaces in between and return the result
-  return lines.join(' ');
-}
-
-// Example usage
-const inputText = `Lorem ipsum dolor sit amet,
-consectetur adipiscing elit,
-sed do eiusmod tempor incididunt`;
-
-const result = addSpacesBetweenLines(inputText);
-console.log(result);
+// Utilizamos la función console.log() para imprimir el mensaje
+console.log(mensaje);
+document.write ("¡Hola");

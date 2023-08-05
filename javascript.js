cb6f0408c8f9e0d187cb75008a5b84bd3f4a286d
DanteDeFlorencia <140614068+Dantedeflorencia@users.noreply.github.com>
2023-08-05 20:02:23 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+/// Definimos el mensaje que queremos imprimir
+var mensaje = "¡Hola, este es un mensaje en pantalla!";
+
+// Utilizamos la función console.log() para imprimir el mensaje
+console.log(mensaje);
+document.write (me);

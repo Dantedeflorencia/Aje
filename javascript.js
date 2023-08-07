3457af44e8ad31da1cd0849da97af3032f1a753c
DanteDeFlorencia <140614068+Dantedeflorencia@users.noreply.github.com>
2023-08-07 12:38:46 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-/// Definimos el mensaje que queremos imprimir
-var mensaje = "¡Hola, este es un mensaje en pantalla!";
-
-// Utilizamos la función console.log() para imprimir el mensaje
-console.log(mensaje);
-document.write (me);
